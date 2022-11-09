ea94c153f34f0e865bbbdaea0facaf517548283c
SalimmReza <salimnuaa@gmail.com>
2022-11-09 23:05:32 +0600

    footer done

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -4,6 +4,7 @@ import { AuthContext } from './Context/AuthProv';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser } from '@fortawesome/free-solid-svg-icons'
 import ClipLoader from "react-spinners/ClipLoader";
+import logo from './Assets/deer.png'
 
 const Nav = () => {
 
@@ -25,20 +26,25 @@ const Nav = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a>Item 333</a></li>
-                        <li tabIndex={0}>
-                            <a className="justify-between">
-                                Parent
-                                <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
-                            </a>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><a>Item 3</a></li>
+                        <li><Link to="/login">Login</Link></li>
+                        <li><Link to='/blog'>Blog</Link></li>
+                        {
+                            user?.email ?
+                                <>
+                                    <li><Link to='/reviews'>My Reviews</Link></li>
+                                    <li><Link to='/addservice'>Add Service</Link></li>
+
+                                    <li><Link><button onClick={handleLogout}
+                                        className="btn btn-ghost">LogOut</button></Link></li>
+                                </>
+                                :
+                                ""
+
+
+                        }
                     </ul>
                 </div>
+                <img className='h-10' src={logo} alt="" />
                 <Link to='/' className="btn btn-ghost normal-case text-xl text-blue-800">Exploria</Link>
             </div>
             <div className="navbar-center hidden lg:flex">
